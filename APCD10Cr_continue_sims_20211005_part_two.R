462afc242670c68d07f231a4454f32fd0e6e9776
Bryce <51974245+bryce-carson@users.noreply.github.com>
2023-10-24 15:56:42 -0600

    Update APCD10Cr_continue_sims_20211005_part_two.R

--- APCD10Cr_continue_sims_20211005_part_two.R
+++ APCD10Cr_continue_sims_20211005_part_two.R
@@ -2,9 +2,9 @@
 # Author: Bryce Carson <bcars268@mtroyal.ca>
 # URL: https://github.com/bryce-carson/APCD10Cr_Carson_2022
 #
-# continueSimulations-2021-10-05-partTwo.R is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# APCD10Cr_continue_sims_20211005_part_two.R is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 #
-# continueSimulations-2021-10-05-partTwo.R is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# APCD10Cr_continue_sims_20211005_part_two.R is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
 
